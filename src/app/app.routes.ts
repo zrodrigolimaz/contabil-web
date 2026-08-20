e2b2c05954a18066f3779d47ff3830fba0a5f8d4
zrodrigolimaz <zrodrigolimaz@gmail.com>
2026-08-20 00:01:13 -0300

    chore: acerta o título da aba

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    title: 'Outros Créditos/Débitos',
+    title: 'Outros Créditos/Débitos — Contábil Web',
     loadComponent: () =>
       import('./features/outros-creditos-debitos/consulta-lotes/consulta-lotes').then(
         (m) => m.ConsultaLotes,
